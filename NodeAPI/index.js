f4cfcf35431aeb576148a748b092f5917257837f
yohiji-1 <yohiji4273@noihse.com>
2026-04-22 13:54:50 +0700

    Added POST PUT DELETE APIs

--- NodeAPI/index.js
+++ NodeAPI/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/search', (req, res) => {
     res.send('<html><body>INSIDE SEARCH API.</body></html>');
 });
 
 app.get('/view', (req, res) => {
     res.send('<html><body>INSIDE VIEW API.</body></html>');
+});
+
+app.post('/login', (req, res) => {
+    res.send('<html><body>INSIDE LOGIN API.</body></html>');
+});
+
+app.put('/updateprofile', (req, res) => {
+    res.send('<html><body>INSIDE UPDATE PROFILE API.</body></html>');
+});
+
+app.delete('/del', (req, res) => {
+    res.send('<html><body>INSIDE DELETE API.</body></html>');
 });
 
 app.listen(5000, '0.0.0.0', () => {
